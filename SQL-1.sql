61744ed3c378f99a29e7818d189e98006165816c
novirariski02 <140144421+novirariski02@users.noreply.github.com>
2023-09-26 19:54:52 +0700

    Update SQL-1.sql

--- SQL-1.sql
+++ SQL-1.sql
@@ -1,21 +1,4 @@
---INPUT
-CREATE TABLE input_table  (
-    ID SERIAL PRIMARY KEY,
-    num VARCHAR(1) NOT NULL
 
-INSERT INTO input_table (num)
-VALUES
-    (1),
-    (2),
-    (2),
-    (2),
-    (2),
-    (1),
-    (1),
-    (1),
-    (1);
- 
---OUTPUT
 SELECT DISTINCT num
 FROM (    
     SELECT num,
