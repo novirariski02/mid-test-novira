78b06ef9aa3c785348db7ab506f8e3daab86d96e
novirariski02 <140144421+novirariski02@users.noreply.github.com>
2023-09-26 19:51:47 +0700

    SQL

---
+++ SQL-1.sql
@@ -0,0 +1,25 @@
+--INPUT
+CREATE TABLE input_table  (
+    ID SERIAL PRIMARY KEY,
+    num VARCHAR(1) NOT NULL
+
+INSERT INTO input_table (num)
+VALUES
+    (1),
+    (2),
+    (2),
+    (2),
+    (2),
+    (1),
+    (1),
+    (1),
+    (1);
+ 
+--OUTPUT
+SELECT DISTINCT num
+FROM (    
+    SELECT num,
+           ROW_NUMBER() OVER () - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS out1
+    FROM input_table) as a
+GROUP BY num, out1
+HAVING COUNT(*) >= 3;
