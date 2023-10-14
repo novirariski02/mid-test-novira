e164954a8e48e318a782eedd3fcf75b88ce587d2
novirariski02 <140144421+novirariski02@users.noreply.github.com>
2023-10-15 05:43:45 +0700

    Add files via upload

---
+++ Python-1.py
@@ -0,0 +1,15 @@
+def add_str(input_str):
+    new_number = ''.join(input_string.split(','))
+    new_number = str(int(new_number) + 1)
+
+    if new_number[0] == '0':
+        new_number = '1' + new_number[1:]
+
+    output_string = ((','), join(new_number))
+
+    return output_string
+
+# Example usage:
+input_string = input("Masukkan angka: ")
+result = add_str(input_string)
+print(result)
